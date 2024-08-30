32c5c92151b21cbcc3960ef645b1134cbef38431
Felipe Sales <soufeliposales@gmail.com>
2024-08-29 21:44:44 -0300

    chore: created first component

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,10 @@
+export function Tweet() {
+    return (
+        <div>
+            <h1>
+                Tweet
+                <button onClick={() => console.log('clicked')}>Click me</button>
+            </h1>
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Tweet } from './components/Tweet'
+
+const editUser = true
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
-    <h1>Hello, world!</h1>
+    {/* <h1>Hello, world!</h1> */}
+    <div>
+      {/* <h1>Hello, world!</h1> */}
+      {/* <button onClick={() => console.log('clicked')}>Click me</button> */}
+      {/* <button>{editUser ? 'Save' : 'Edit'}</button> */}
+      <Tweet />
+      <Tweet />
+    </div>
   </StrictMode>,
 )
