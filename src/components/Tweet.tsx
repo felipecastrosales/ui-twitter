59b793c74ca91cb26fb98f5bb70f7283a1f73a86
Felipe Sales <soufeliposales@gmail.com>
2024-08-31 22:12:04 -0300

    chore: added styles and flows to inline styles

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -4,11 +4,18 @@ interface TweetProps {
   likes?: number;
 }
 
+const tweetStyles = {
+  backgroundColor: 'black',
+  borderRadius: 8
+};
+
 // export function Tweet(props) {
 export function Tweet(props: TweetProps) {
   // console.log(props)
   return (
-    <div>
+    // <div className="tweet">
+    <div className="tweet" style={{ backgroundColor: 'black', borderRadius: 8 }}>
+    {/* <div className="tweet" style={tweetStyles}> */}
       {/* {props.user} */}
       <strong>{props.user}</strong>
       {/* <p>{props.content}</p> */}
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Tweet } from './components/Tweet'
 
+import './global.css'
+
 // typescript -> tool for static type checking
 // java, dart... 
 // static type checking -> type checking at compile time
