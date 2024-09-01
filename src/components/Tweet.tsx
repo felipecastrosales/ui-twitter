adf897237a2ec5adfd232bb6e5fca99562a73b59
Felipe Sales <soufeliposales@gmail.com>
2024-08-31 21:57:25 -0300

    style: format and change identation

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,16 +1,17 @@
+// export function Tweet(props) {
 export function Tweet(props) {
-    // console.log(props)
-    return (
-        <div>
-            {/* {props.user} */}
-            <strong>{props.user}</strong>
-            {/* <p>{props.content}</p> */}
-            <p>{props.children}</p>
-            <button>like</button>
-            {/* <h1> */}
-                {/* Tweet */}
-                {/* <button onClick={() => console.log('clicked')}>Click me</button> */}
-            {/* </h1> */}
-        </div>
-    );
+  // console.log(props)
+  return (
+    <div>
+      {/* {props.user} */}
+      <strong>{props.user}</strong>
+      {/* <p>{props.content}</p> */}
+      <p>{props.children}</p>
+      <button>like</button>
+      {/* <h1> */}
+          {/* Tweet */}
+          {/* <button onClick={() => console.log('clicked')}>Click me</button> */}
+      {/* </h1> */}
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Tweet } from './components/Tweet'
 
+// typescript -> tool for static type checking
+// java, dart... 
+// static type checking -> type checking at compile time
+//                         when is the code being compiled and developed
+
 const editUser = true
 
 createRoot(document.getElementById('root')!).render(
