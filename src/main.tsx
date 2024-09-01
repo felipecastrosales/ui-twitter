bec20a0ca052b87105706e79e75da0e21ff39ed1
Felipe Sales <soufeliposales@gmail.com>
2024-08-31 22:23:32 -0300

    chore: added styles for application and removed untrackeds; import font

--- src/main.tsx
+++ src/main.tsx
@@ -13,38 +13,5 @@ const editUser = true
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* <h1>Hello, world!</h1> */}
-    <div>
-      {/* <h1>Hello, world!</h1> */}
-      {/* <button onClick={() => console.log('clicked')}>Click me</button> */}
-      {/* <button>{editUser ? 'Save' : 'Edit'}</button> */}
-      {/* <input type="time" /> */}
-      {/* <Tweet />
-      <Tweet /> */}
-      {/* <Tweet user="test" content="Hello, world!" />
-      <Tweet user="test1" content="ok" />
-      <Tweet user="test2" content="hello" /> */}
-
-      {/* <Tweet user="test" content="Hello, world!" />
-      <Tweet user="test1" content="ok" />
-      <Tweet user="test2" content="hello" /> */}
-
-      <Tweet user="test">
-          Hello, world!
-      </Tweet>
-
-      <Tweet user="test1">
-          ok
-      </Tweet>
-      
-      <Tweet user="test2">
-          hello
-      </Tweet>
-
-      <Tweet user="test2" likes={10}>
-          hello
-      </Tweet>
-    </div>
   </StrictMode>,
 )
