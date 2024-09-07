b8b35f359e0307d10c58cbd94bee2dd61c56bb9b
Felipe Sales <soufeliposales@gmail.com>
2024-09-07 11:30:34 -0300

    feat: creating new components for sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -1,58 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Tweet } from './components/Tweet'
-import { Bell, BookmarkSimple, DotsThree, DotsThreeCircle, Envelope, FileText, Hash, House, Sparkle, User } from 'phosphor-react'
+import { Sparkle } from 'phosphor-react'
 
 import './global.css';
-
-import twitterLogo from './assets/logo-twitter.svg'
+import { Sidebar } from './components/Sidebar';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className="layout">
-      <aside className="sidebar">
-        <img className="logo" src={twitterLogo} alt="Twitter" />
-        
-        <nav className="main-navigation">
-          <a className="active" href="">
-            <House weight="fill" size={24} />
-            Home
-          </a>
-          <a href="">
-            <Hash/>
-            Explore
-          </a>
-          <a href="">
-            <Bell />
-            Notifications
-          </a>
-          <a href="">
-            <Envelope />
-            Messages
-          </a>
-          <a href="">
-            <BookmarkSimple />
-            Bookmarks
-          </a>
-          <a href="">
-            <FileText />
-            Lists
-          </a>
-          <a href="">
-            <User />
-            Profile
-          </a>
-          <a href="">
-            <DotsThreeCircle />
-            More
-          </a>
-        </nav>
-        
-        {/* <button>Tweet</button> */}
-        <button className="new-tweet" type="button">
-          Tweet
-        </button>
-      </aside>
+      <Sidebar />
+
       <div className="content">
         <main className="timeline">
           <div className="timeline-header">
